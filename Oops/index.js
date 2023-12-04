7e47bc82c3a5757524d39f0693ddd20e07da8ff4
Sathish-30 <sathish.amsaravanan@gmail.com>
2023-12-04 22:31:55 +0530

    Added the implementaton of oops module

--- Oops/index.js
+++ Oops/index.js
@@ -1,14 +1,34 @@
 // Prototype
 
-let person1 = {
-  name: "sathish",
-  age: 20,
-};
+function Person(name , age){
+  this.name = name;
+  this.age = age;
+  // this.getName = function(){
+  //   console.log(`The Person name is ${this.name} and age is ${this.age}`)
+  // }
+}
 
+// The below function will create the function in the prototype
+Person.prototype.getName = function(){
+  console.log(`The Person name is ${this.name} and age is ${this.age}`)
+}
+
+// The call , apply and bind method are used in oops concept in js
+// call is used to call the fn of a object and apply is used as same but argument are passed as an array
+
+const person1 = new Person("sathish",11);
 let person2 = {
-  name: "rowdy",
-  age: 19,
-};
-console.log(person1);
-console.log(person1.hasOwnProperty("gender"));
-console.log(person2.hasOwnProperty("name"));
+  name : "john",
+  age : 12,
+}
+
+const printDetails = function(city){console.log(`The name is ${this.name} and age is ${this.age} and city is ${city}`)}
+
+let person3 = {
+  name : "rowdy",
+  age : 12,
+}
+printDetails.call(person3 , "Coimbatore");
+
+person1.getName();
+console.log(person2);
